004ae43b1406cba62202584ae34b67b5c3dfdfa5
Shikai <sluo@ncsu.edu>
2016-02-08 15:01:03 -0500

    modify gpu_Python.py

--- Python_summary/gpu_Python.py
+++ Python_summary/gpu_Python.py
@@ -2,7 +2,7 @@ import numpy as np
 import pycuda.gpuarray as gpuarray
 import pycuda.autoinit
 import skcuda.linalg as linalg
-import time       
+import time
 
 v0 = np.loadtxt('V0.txt').astype(np.float32)
 w0 = np.loadtxt('W0.txt').astype(np.float32)
@@ -13,15 +13,15 @@ def NNMF_gpu(X,r,tol,V=v0,W=w0,verbose=1):
     Vr = V[:,0:r].copy()
     Wr = W[0:r,:].copy()
     X_gpu = gpuarray.to_gpu(X)
-    V_gpu = gpuarray.to_gpu(Vr) 
-    W_gpu = gpuarray.to_gpu(Wr) 
-    #Frobinius norm at previous step 
-    B_gpu = linalg.dot(V_gpu, W_gpu) 
-    L = linalg.norm(X_gpu-B_gpu)**2 
+    V_gpu = gpuarray.to_gpu(Vr)
+    W_gpu = gpuarray.to_gpu(Wr)
+    #Frobinius norm at previous step
+    B_gpu = linalg.dot(V_gpu, W_gpu)
+    L = linalg.norm(X_gpu-B_gpu)**2
     iteration = 0
     while 1: #update V
-        V_gpu *= linalg.dot(X_gpu,linalg.transpose(W_gpu)) 
-        V_gpu /= linalg.dot(B_gpu,linalg.transpose(W_gpu)) 
+        V_gpu *= linalg.dot(X_gpu,linalg.transpose(W_gpu))
+        V_gpu /= linalg.dot(B_gpu,linalg.transpose(W_gpu))
         B_gpu = linalg.dot(V_gpu, W_gpu)
         #update W
         W_gpu *= linalg.dot(linalg.transpose(V_gpu),X_gpu)
